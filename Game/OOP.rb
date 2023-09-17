f48d312892e8515ae9ef5850f2ce4a4b5758e76d
nikita <evstyagin09@gmail.com>
2023-09-17 20:35:20 +0300

    ready

--- Game/OOP.rb
+++ Game/OOP.rb
@@ -43,10 +43,12 @@ class RaspberryBush
 end
 
 
-class Human < RaspberryBush
-    def initialize(name)
+class Human
+    attr_accessor :plant
+    protected :plant
+    def initialize(name, plant)
         @name = name
-        @plant = RaspberryBush.new(6)
+        @plant = plant
     end
 
     def work!
@@ -56,26 +58,50 @@ class Human < RaspberryBush
     def harvest
         if @plant.ripe_all?
             @plant.give_away_all!
-            p "урожай собран #{@name}"
             return
         end
-        p "РАНО"
+        p "EARLY"
     end
 
     def self.knowledge_base
         puts "Самый лучший срок посадки осенью, перед заморозками, тогда весной, возможно,
-         уже можно будет попробовать первый урожай. Конечно, он не будет обильным, но все же. 
-         Высаживают малину на глубину около 35 сантиметров, на расстоянии друг от друга — 50 см."
+уже можно будет попробовать первый урожай. Конечно, он не будет обильным, но все же. 
+Высаживают малину на глубину около 35 сантиметров, на расстоянии друг от друга — 50 см."
     end
 end
-
+=begin
 if __FILE__ == $PROGRAM_NAME
+    
+
     Human.knowledge_base
     bush = RaspberryBush.new(5)
-    human = Human.new("Nikita")
-    human.work!
-    
-    human.harvest
+    human = Human.new("Nikita", bush)
     human.work!
     human.harvest
+    human.work!
+    human.harvest
+end
+=end
+
+if __FILE__ == $PROGRAM_NAME
+    p "What's your name?"
+    name = gets.chop!
+    bush = RaspberryBush.new(10)
+    human = Human.new(name, bush)
+    puts "Hi, #{@name}! Now you have 10 raspberry bushes. You can:\n1. Harvest raspberries\n2. Work to grow bushes\n3. Know your knowledge base\n4. Exit"
+
+    while (n = gets.chop!.to_i) != 4
+        case n
+        when 1
+            human.harvest
+            p "You win"
+        when 2
+            human.work!
+            p "Good job"
+        when 3
+            Human.knowledge_base
+        else
+            p "You entered an incorrect number"
+        end
+    end
 end
